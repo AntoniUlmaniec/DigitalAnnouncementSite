98b7f2e1c22b0447703b37aeb3cc1358e847c4a5
Antoni <antekulmaniec0@gmail.com>
2024-12-30 15:32:15 +0100

    poczatki klas

---
+++ site_project/src/Announcement.java
@@ -0,0 +1,43 @@
+public class Announcement {
+    private int id;
+    private String title;
+    private String content;
+    private User owner;
+
+    public void addComment(User user, String comment) {
+        // do dokoczenia
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
+
